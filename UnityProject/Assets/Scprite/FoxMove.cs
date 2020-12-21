6f1f0fee0be3676f00639df9e203df5e418ec159
mch0520 <ch052088@gmail.com>
2020-12-21 19:41:26 +0800

    speed

--- UnityProject/Assets/Scprite/FoxMove.cs
+++ UnityProject/Assets/Scprite/FoxMove.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class FoxMove : MonoBehaviour
 {
 	//狐狸的移動速度
-	public float speed;
+	public float speed=10;
 	//箱子的圖片渲染器
 	public SpriteRenderer mySpriteR;
 	//狐狸
 	public Rigidbody2D fox;
+	//速度修改器
+    public float sp;
 
-	public void Start()
+    public void Start()
 	{
 		mySpriteR = GetComponent<SpriteRenderer>();
 		fox = GetComponent<Rigidbody2D>();
@@ -20,17 +22,21 @@ public class FoxMove : MonoBehaviour
 	}
 	public void Move(float speed)
 	{
-		if(Input.GetKey(KeyCode.A)&Input.GetKey(KeyCode.LeftArrow))
+		if(Input.GetKey(KeyCode.A) | Input.GetKey(KeyCode.LeftArrow))
 		{
-			speed *= Input.GetAxisRaw("Horizontal");
+			sp = speed;
+			speed = sp*Input.GetAxisRaw("Horizontal");
 			mySpriteR.flipX = true;
 			fox.AddForce(new Vector2(speed, 0));
+			print("a or Left" + speed);
 		}
-		else if(Input.GetKey(KeyCode.D)&Input.GetKey(KeyCode.RightArrow))
+		if(Input.GetKey(KeyCode.D) | Input.GetKey(KeyCode.RightArrow))
         {
-			speed *= Input.GetAxisRaw("Horizontal");
+			sp = speed;
+			speed = sp*Input.GetAxisRaw("Horizontal");
 			mySpriteR.flipX = false;
 			fox.AddForce(new Vector2(speed, 0));
+			print("d or Right"+ speed);
 		}
 		
 	}
@@ -39,13 +45,8 @@ public class FoxMove : MonoBehaviour
 	public void Update()
 	{
 	   Move(speed);
-		if(Input.anyKey)
-        {
-			print("input success");
-        }
-		if(Input.GetKey("a"))
-        {
-			print("input a");
-        }
+		print(Input.GetAxisRaw("Horizontal"));
+		print(speed);
+
 	}
 }
